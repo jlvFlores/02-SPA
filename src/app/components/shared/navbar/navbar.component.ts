904c128c82a78cc47c531fb6afb3aa90da266fb4
Jose Luis <jluisvazquez.jv@gmail.com>
2020-09-23 11:34:45 -0500

    64 buscador de heroes

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -11,4 +11,7 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  buscarHeroe( termino: string ){
+    console.log(termino);
+  }
 }
--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -69,6 +69,23 @@ export class HeroesService {
         return this.heroes[idx];
     }
 
+    buscarHeroes( termino: string ): Heroe[]{
+
+        let heroesArr: Heroe[] = [];
+        termino = termino.toLowerCase();
+
+        for ( let heroe of this.heroes) {
+
+            let nombre = heroe.nombre.toLowerCase();
+
+            if ( nombre.indexOf( termino ) >= 0 ){
+                heroesArr.push( heroe );
+            }
+        }
+
+        return heroesArr;
+    }
+
 }
 
 export interface Heroe{
