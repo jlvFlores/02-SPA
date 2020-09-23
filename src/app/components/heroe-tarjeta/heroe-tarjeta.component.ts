ba96e691db0093f05baaac44482035b969ed37a8
Jose Luis <jluisvazquez.jv@gmail.com>
2020-09-23 15:05:09 -0500

    70 Arreglamos el corraje que me dejo

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -22,8 +22,8 @@ export class HeroeTarjetaComponent implements OnInit {
 
   verHeroe() {
   //   console.log( this.index );
-  //   this.router.navigate( ['/heroe', this.index] );
-    this.heroeSeleccionado.emit( this.index );
+    this.router.navigate( ['/heroe', this.index] );
+  //  this.heroeSeleccionado.emit( this.index );
   }
 
 }
--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -74,11 +74,14 @@ export class HeroesService {
         let heroesArr: Heroe[] = [];
         termino = termino.toLowerCase();
 
-        for ( let heroe of this.heroes) {
+        for ( let i = 0; i < this.heroes.length; i ++ ) {
+
+            let heroe = this.heroes[i];
 
             let nombre = heroe.nombre.toLowerCase();
 
             if ( nombre.indexOf( termino ) >= 0 ) {
+                heroe.idx = i;
                 heroesArr.push( heroe );
             }
         }
@@ -94,4 +97,5 @@ export interface Heroe{
     img: string;
     aparicion: string;
     casa: string;
+    idx?: number;
 }
