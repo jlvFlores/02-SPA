6f421cf2be8c32cc32fa95571111ae5e91b56701
Jose Luis <jluisvazquez.jv@gmail.com>
2020-09-23 14:56:36 -0500

    69 Output

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,14 +11,19 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe: any = {};
   @Input() index: number;
 
-  constructor( private router: Router) { }
+  @Output() heroeSeleccionado: EventEmitter<number>;
+
+  constructor( private router: Router) {
+    this.heroeSeleccionado = new EventEmitter();
+  }
 
   ngOnInit(): void {
   }
 
   verHeroe() {
-    console.log( this.index );
-    this.router.navigate( ['/heroe', this.index] );
+  //   console.log( this.index );
+  //   this.router.navigate( ['/heroe', this.index] );
+    this.heroeSeleccionado.emit( this.index );
   }
 
 }
